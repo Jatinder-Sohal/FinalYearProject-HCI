9af321f2f26eecc15b436831d0594c5bfbd80366
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2023-10-26 16:58:45 +0100

    Adding get started component, background image and moved css page down into src so I can apply to singular files instead of the whole application.

---
+++ finance_tracker/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import GetStarted from "./components/GetStarted";
+
+function App(){
+    return <div>
+        <Navbar />
+        <GetStarted />
+    </div>
+}
+
+export default App;
