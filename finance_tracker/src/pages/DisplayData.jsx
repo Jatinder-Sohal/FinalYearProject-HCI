1e008808076bf025323eb1a67e9aca5f79cfe9a4
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2023-10-28 14:37:00 +0100

    Added text to the page and make changes to the layout, background and colour of the page. I have done this using visual heirarchies and designing with user in focus.

--- finance_tracker/src/pages/DisplayData.jsx
+++ finance_tracker/src/pages/DisplayData.jsx
@@ -2,13 +2,47 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Carousel from "../components/Carousel";
 
-function DisplayData(){
-    return <div>
+function DisplayData() {
+  return (
+    <div>
         <Navbar />
-        <div class="custom-container">
-        <Carousel />
+        <h1 className="title">Some Recommendations on How to Cut Costs</h1>
+        <div className="custom-container">
+            <Carousel />
+        </div>
+        <div className="content">
+        <section className="section">
+    <h2 className="section-title">Cutting Bills</h2>
+    <p>Here are some tips on how to reduce your bills:</p>
+    <ul className="recommendation-list">
+      <li>Switch to energy-efficient appliances or natural energy sources.</li>
+      <li>Use smart thermostats to regulate heating and cooling.</li>
+      <li>Shop for better insurance rates e.g using moneysupermarket.</li>
+    </ul>
+  </section>
+
+  <section className="section">
+    <h2 className="section-title">Cutting Food Expenses</h2>
+    <p>Consider these strategies to save on food expenses:</p>
+    <ul className="recommendation-list">
+      <li>Create a list of groceries and stick to budget.</li>
+      <li>Plan meals in advance to avoid eating out frequently.</li>
+      <li>Look for discounts and coupons when shopping.</li>
+    </ul>
+  </section>
+
+  <section className="section">
+    <h2 className="section-title">Other Sources of Income</h2>
+    <p>Explore additional income options:</p>
+    <ul className="recommendation-list">
+      <li>Take on a part-time job or freelance work.</li>
+      <li>Rent out a spare room or property on platforms like Airbnb.</li>
+      <li>Invest in stocks or other income-generating assets.</li>
+    </ul>
+  </section>
         </div>
     </div>
+  );
 }
 
 export default DisplayData;
