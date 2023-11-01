6d85cc04baf1ab7740d5a2ddf2a9a8172ada305f
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2023-11-01 14:28:45 +0000

    Experimted different styles, and now created a title box for main page with styling.

--- notes_application/src/main/java/notes_application/App.java
+++ notes_application/src/main/java/notes_application/App.java
@@ -1,12 +1,11 @@
 package notes_application;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * JavaFX App
@@ -17,8 +16,9 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("primary"), 1200, 800);
         stage.setScene(scene);
+        scene.getStylesheets().add(getClass().getResource("main.css").toExternalForm());
         stage.show();
     }
 
