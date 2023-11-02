e928f6c2fef15638493c797778f689f5d8f3ce05
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2023-11-02 17:05:49 +0000

    Added css to each title and centered it correctly on top of list. Also changes colour scheme of page.

--- notes_application/src/main/java/notes_application/App.java
+++ notes_application/src/main/java/notes_application/App.java
@@ -16,7 +16,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 1200, 800);
+        scene = new Scene(loadFXML("primary"), 1600, 800);
         stage.setScene(scene);
         scene.getStylesheets().add(getClass().getResource("main.css").toExternalForm());
         stage.show();
