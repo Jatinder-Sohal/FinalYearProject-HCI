4e59c59a042afd2941312b108b0b51a038c08eb9
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2023-11-08 13:30:30 +0000

    Added 2 cards to list by creating an add card function that creates a new frame ontop of the list and adding a title plus content to the card.

--- notes_application/Main.py
+++ notes_application/Main.py
@@ -28,23 +28,42 @@ def create_navbar():
     dropdown.place(relx=0.93, rely=0.05, anchor=tk.CENTER)
 
 
-def create_card(title):
-    card_frame = ctk.CTkFrame(root, height=700, width=275, corner_radius=10)
+def create_list(title):
+    card_frame = ctk.CTkFrame(root, height=700, width=275, fg_color='white', corner_radius=10)
     card_frame.place(x=200, y=100)
     #NOTE - stops content affecting size of frame
     card_frame.pack_propagate(False)
 
-    border_frame = ctk.CTkFrame(card_frame, fg_color='blue', height=5)
+    border_frame = ctk.CTkFrame(card_frame, fg_color='grey', height=5)
     border_frame.pack(side="top", fill="x")
 
-    title_label = ctk.CTkLabel(card_frame, text=title)
-    title_label.pack(pady=(6, 10))
+    title_label = ctk.CTkLabel(card_frame, text=title,font=("Arial Bold", 25))
+    title_label.pack(pady=(6, 20))
     
-
     return card_frame
 
 
+def create_card(list_frame, card_title, card_content):
+    card = ctk.CTkFrame(list_frame, width=250, height=100, corner_radius=10)  
+    card.pack(padx=10, pady=10, fill='x', expand=False)  
+
+    title = ctk.CTkLabel(card, text=card_title, font=("Arial", 20), anchor="w", width=200)
+    title.pack(side='top', fill='x', padx=25, pady=(2, 5))
+
+    content = ctk.CTkLabel(card, text=card_content, font=("Arial", 16), wraplength=230) 
+    content.pack(fill='x', padx=5, pady=2)
+
+    return card
+
+
+
 create_navbar()
-card = create_card("To do")
+list = create_list("To-do")
+
+create_card(list, "Card Title", "This is the content of the card. testing extra chars")
+create_card(list, "Card Title", "This is the content of the card. testing extra chars")
+
+
+
 
 root.mainloop()
