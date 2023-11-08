c6fc683ac7169acd15838e4ea656e38253cc9686
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2023-11-08 12:37:24 +0000

    Created a list to store cards and added a fake border to the top of the list to improve looks. Had to fix bug where contents controlled height and width of list.

--- notes_application/Main.py
+++ notes_application/Main.py
@@ -6,25 +6,45 @@ ctk.set_appearance_mode("light")
 
 root = ctk.CTk()
 root.title("Project planning")
-root.geometry("1200x800+300+100")
+root.geometry("1400x900+300+50")
 root.configure(bg_color="#F9F7F7")
 
 
+def create_navbar():
+    navbar = tk.Frame(root, height=75, bg='white') 
+    navbar.pack(side=tk.TOP, fill=tk.X)
 
-navbar = tk.Frame(root, height=75, bg='white') 
-navbar.pack(side=tk.TOP, fill=tk.X)
+    label = tk.Label(root, text = "Project board", bg='white', font=("Arial", 20))
+    label.place(relx = 0.5, rely = 0.05, anchor = tk.CENTER)
 
-label = tk.Label(root, text = "Project board", bg='white', font=("Arial", 20))
-label.place(relx = 0.5, rely = 0.06, anchor = tk.CENTER)
-
-button = ctk.CTkButton(root, text="Share", fg_color="black",text_color="white", font=("Arial", 20), hover_color="grey", width=80, height=30)                    
-button.place(relx=0.845, rely=0.06, anchor=tk.CENTER)
+    button = ctk.CTkButton(root, text="Share", fg_color="black",text_color="white", font=("Arial", 20), hover_color="grey", width=80, height=30)                    
+    button.place(relx=0.859, rely=0.05, anchor=tk.CENTER)
 
 
-options = ["PNG", "JPG", "Image"]
-dropdown = ctk.CTkOptionMenu(root, values=options, width=100, fg_color="black", button_color='black',font=("Arial", 20))
-dropdown.pack(pady=20)
-dropdown.set("Export")
-dropdown.place(relx=0.93, rely=0.06, anchor=tk.CENTER)
+    options = ["PNG", "JPG", "Image"]
+    dropdown = ctk.CTkOptionMenu(root, values=options, width=100, fg_color="black", button_color='black',font=("Arial", 20))
+    dropdown.pack(pady=20)
+    dropdown.set("Export")
+    dropdown.place(relx=0.93, rely=0.05, anchor=tk.CENTER)
+
+
+def create_card(title):
+    card_frame = ctk.CTkFrame(root, height=700, width=275, corner_radius=10)
+    card_frame.place(x=200, y=100)
+    #NOTE - stops content affecting size of frame
+    card_frame.pack_propagate(False)
+
+    border_frame = ctk.CTkFrame(card_frame, fg_color='blue', height=5)
+    border_frame.pack(side="top", fill="x")
+
+    title_label = ctk.CTkLabel(card_frame, text=title)
+    title_label.pack(pady=(6, 10))
+    
+
+    return card_frame
+
+
+create_navbar()
+card = create_card("To do")
 
 root.mainloop()
