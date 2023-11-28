111e333a2064fd74a51b4ac848d6479db282bb96
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2023-11-28 20:56:05 +0000

    Implemented new functionality, so that when save button pressed, the sheet name will be stored in the sidebar. This required me to pass functions as props and use a simple event handler to record what is being written and then added to the spread out array.

--- finance_tracker/src/components/Sidebar.jsx
+++ finance_tracker/src/components/Sidebar.jsx
@@ -1,34 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import binIcon from '../images/Bin.png'; 
 
-function Sidebar() {
-    const initialSheets = ['Sheet 1', 'December 2021', 'Sheet 2', 'February 2022', 'October 2023', 'Sheet 2', 'January 2023', 'December 2022'];
-    const [sheets, setSheets] = useState(initialSheets);
-
-    function removeSheet(sheetBeingRemoved) {
-        setSheets(sheets.filter(function(sheet) {
-            return sheet !== sheetBeingRemoved;
-        }));
-    }
-
+function Sidebar({sheets, RemoveSheet}) {
     return (
         <div className="sidebar">
             <div className="sidebar-content">
                 <h1 className="sidebar-heading">Your sheets</h1>
                 <ul className="sheet-list">
-                    {sheets.map(function(sheet, index) {
-                        return (
-                            <li key={index} className="sheet-item">
-                                {sheet}
-                                <img 
-                                    src={binIcon} 
-                                    alt="Delete sheet in sidebar" 
-                                    className="sheet-delete" 
-                                    onClick={() => removeSheet(sheet)} 
-                                />
-                            </li>
-                        );
-                    })}
+                    {sheets.map((sheet, index) => (
+                        <li key={index} className="sheet-item">
+                            {sheet}
+                            <img 
+                                src={binIcon} 
+                                alt="Delete sheet" 
+                                className="sheet-delete"
+                                onClick={() => RemoveSheet(sheet)} 
+                            />
+                        </li>
+                    ))}
                 </ul>
             </div>
         </div>   
--- finance_tracker/src/pages/EnterData.jsx
+++ finance_tracker/src/pages/EnterData.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import React from "react";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
@@ -6,13 +7,28 @@ import SheetHeading from "../components/SheetHeading";
 
 import './EnterData.css';
 
-function EnterData(){
-    return <div className="page3-container">
-        <Navbar />
-        <Sidebar />
-        <SheetHeading />
-        <ExpenceTable />
-    </div>
+function EnterData() {
+    const initialSheets = ['Sheet 1', 'December 2021', 'Sheet 2', 'February 2022', 'October 2023', 'Sheet 3', 'January 2023', 'December 2022'];
+    const [sheets, setSheets] = useState(initialSheets);
+
+    const addSheet = (newSheetName) => {
+        if (newSheetName) {
+            setSheets([...sheets, newSheetName]);
+        }
+    };
+
+    const removeSheet = (sheetBeingRemoved) => {
+        setSheets(sheets.filter(sheet => sheet !== sheetBeingRemoved));
+    };
+
+    return (
+        <div className="page3-container">
+            <Navbar />
+            <Sidebar sheets={sheets} RemoveSheet={removeSheet} />
+            <SheetHeading AddSheet={addSheet} />
+            <ExpenceTable />
+        </div>
+    );
 }
 
 export default EnterData;
