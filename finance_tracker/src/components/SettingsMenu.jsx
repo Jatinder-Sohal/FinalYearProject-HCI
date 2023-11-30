fd45dbc72de0f00c2016b16be27c724f77250e59
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2023-11-30 22:25:22 +0000

    Restyled dropdown and added text to it including title, subheadings and checked boxs with labels. Started implementing functionality, by enlarging text when box is clicked.

--- finance_tracker/src/components/SettingsMenu.jsx
+++ finance_tracker/src/components/SettingsMenu.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import { useState } from 'react';
 import SettingImage from '../images/settings-svgrepo-com.png';
 import './SettingsMenu.css';
 
 function SettingsMenu() {
+const [isTextEnlarged, setTextEnlarged] = useState(false);
+
+  const toggleTextSize = () => {
+    setTextEnlarged(!isTextEnlarged);
+    document.body.classList.toggle('enlarge-text');
+  };
   return (
     <div className="settings-container">
       <img src={SettingImage} className="setting-size" alt="Settings" />      
         <div className="settings-dropdown">
-            <h4>Visual Impairment</h4>              
+            <h3 className="dropdown-title">Accessibility Settings</h3>
+            <h4>Visual Impairment</h4>   
+                <label>
+                    <input type="checkbox" className='checkbox' /> High Contrast Mode
+                </label>
+                <label>
+                    <input type="checkbox" className='checkbox' checked={isTextEnlarged} onChange={toggleTextSize} /> Enlarge Text
+                </label>
+                <h4>Color Blindness</h4>
+                <label>
+                    <input type="checkbox" className='checkbox' /> Deuteranopia (Green-Blind)
+                </label>
+                <label>
+                    <input type="checkbox" className='checkbox' /> Protanopia (Red-Blind)
+                </label>
+                <h4>Audio Impairment</h4>
+                <label>
+                    <input type="checkbox" className='checkbox' /> Screen Reader
+                </label>            
         </div>      
     </div>
   );
