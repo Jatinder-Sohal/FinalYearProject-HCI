a7ef351d016781709968909d6bb8ee8e81ab276d
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2023-12-03 13:22:57 +0000

    Added comments to the Navbar class and its testing classes.

--- notes_application/main/navbar.py
+++ notes_application/main/navbar.py
@@ -4,17 +4,20 @@ import customtkinter as ctk
 from PIL import Image, ImageTk
 from tktooltip import ToolTip
 
-
+#Class which creates the navbar and the elements inside it
 class Navbar:
+    #Initialses the root and other elements and calls the methods
     def __init__(self, root, open_add_card_window, context):
         self.root = root
         self.open_add_card_window = open_add_card_window
+        self.context = context
+        #The 2 methods
         self.create_navbar()
         self.top_bar = self.create_top_bar()
-        self.context = context
+        #Created for testing
         self.help_window_open = False 
 
-        
+    #Creates the actual white bar, the title and the buttons on right    
     def create_navbar(self):
         navbar = tk.Frame(self.root, height=75, bg='#F9F7F7') 
         navbar.pack(side=tk.TOP, fill=tk.X)
@@ -23,16 +26,15 @@ class Navbar:
         label.place(relx = 0.5, rely = 0.05, anchor = tk.CENTER)
 
 
-
+        #Help button with question mark image attached on the right. Calls last method when pressed
         help_img = Image.open("../resources/help-alt-svgrepo-com.png")
-
         help_photo = ctk.CTkImage(help_img)
-
         help_button = ctk.CTkButton(self.root, text="Help", hover_color="grey", image=help_photo, compound=tk.RIGHT, width=50, height=30, font=("Arial", 18), text_color="white",  fg_color="#3F72AF", command=self.show_help_window) 
         help_button.place(relx=0.71, rely=0.05, anchor=tk.CENTER)
         ToolTip(help_button, msg="Click this button to get information")
 
 
+        #Export dropdown
         options = ["PNG", "JPG", "Image"]
         dropdown = ctk.CTkOptionMenu(self.root, values=options, width=100, height=30, text_color="white", fg_color="#3F72AF", button_color='#3F72AF',font=("Arial", 18))
         dropdown.pack(pady=20)
@@ -40,16 +42,16 @@ class Navbar:
         dropdown.place(relx=0.781, rely=0.05, anchor=tk.CENTER)
         ToolTip(dropdown, msg="Click to export board as an image")
 
-
+        #Search bar
         search_entry = ctk.CTkEntry(self.root, width=235, placeholder_text="search", font=("Arial", 18))
         search_entry.place(relx=0.91, rely=0.05, anchor=tk.CENTER)
         ToolTip(search_entry, msg="Search for a setting here")
-        
+
+        #Attaching the search icon to the search bar
         search_image = Image.open("../resources/magnifier-left-svgrepo-com.png")
         search_image = search_image.resize((19, 19), Image.ANTIALIAS)
+        #Resizing image and placing on right
         self.root.search_image = ImageTk.PhotoImage(search_image)
-
-        
         search_image_label = tk.Label(self.root, image=self.root.search_image, bg="white")
         search_image_label.image = self.root.search_image
         search_image_label.place(relx = 0.98, rely=0.05, anchor = tk.CENTER)
@@ -57,28 +59,28 @@ class Navbar:
 
         return navbar
 
+    #Creates the elements on the left of the navbar
     def create_top_bar(self):
         top_bar = tk.Frame(self.root, bg='white') 
         top_bar.place(relx = 0.13, rely = 0.05, anchor = tk.CENTER)
         
+        #Add card button with icon attached
         add_img = Image.open("../resources/add-circle-svgrepo-com.png")
         add_photo = ctk.CTkImage(add_img)
-        
-        undo_img = Image.open("../resources/undo-svgrepo-com.png")
-        undo_photo = ctk.CTkImage(undo_img)
-
-        redo_img = Image.open("../resources/redo-svgrepo-com.png")
-        redo_photo = ctk.CTkImage(redo_img)
-
         add_card_button = ctk.CTkButton(top_bar, text="Add card", hover_color="grey", image=add_photo, width=100, height=30, font=("Arial", 18), text_color="white", command=self.open_add_card_window, fg_color="#3F72AF")
         add_card_button.pack(side=tk.LEFT)
         ToolTip(add_card_button, msg="Click this button to add a new card")
 
-
+        #Undo button with icon attached
+        undo_img = Image.open("../resources/undo-svgrepo-com.png")
+        undo_photo = ctk.CTkImage(undo_img)
         undo_button = ctk.CTkButton(top_bar, text="Undo", image=undo_photo, hover_color="grey", text_color="white", width=100, height=30, font=("Arial", 18), fg_color="#3F72AF", command=lambda: self.context.action_history.undo(self.context))
         undo_button.pack(side=tk.LEFT, padx=10)
         ToolTip(undo_button, msg="Click this button to undo any changes")
 
+        #Redo button with icon attached
+        redo_img = Image.open("../resources/redo-svgrepo-com.png")
+        redo_photo = ctk.CTkImage(redo_img)
         redo_button = ctk.CTkButton(top_bar, text="Redo", image=redo_photo, hover_color="grey", text_color="white", width=100, height=30, font=("Arial", 18), fg_color="#3F72AF", command=lambda: self.context.action_history.redo(self.context))
         redo_button.pack(side=tk.LEFT)
         ToolTip(redo_button, msg="Click this button to redo any changes")
@@ -86,8 +88,10 @@ class Navbar:
         return top_bar
 
 
+    #Method which displays help window when button pressed
     def show_help_window(self):
             self.help_window_open = True
+            #Creates a top level window - above the root window
             help_window = tk.Toplevel(self.root)
             help_window.title("Help and Support")
             help_window.geometry("600x500+630+140") 
@@ -105,6 +109,7 @@ class Navbar:
             
             Facebook: @ProjectPlanningUK
             """
+            #Displays the text above in the text attribute
             contact_text = tk.Text(help_window, height=10, bg="gainsboro", width=150, font=("Arial Bold", 22))
             contact_text.insert("end", contact_details)
             contact_text.config(state="disabled")  
--- notes_application/test/testNavbarFunction.py
+++ notes_application/test/testNavbarFunction.py
@@ -10,41 +10,48 @@ from new_card import New_Card
 from history import Action, ActionHistory
 from navbar import Navbar
 
+#Class which tests the functionality of the navbar and its buttons
 class TestNavbarFunction(unittest.TestCase):
+    #Set ups the root and action history to track changes
     def setUp(self):
         self.root = tk.Tk()
+        #Don't need the other return values so use *_
         self.to_do_list, *_ = Lists.create_list("To-do", 100, 'grey', self.root)
         self.to_do_cards = []
         self.action_history = ActionHistory() 
-
+        #Attaches the context to action history
         self.context = CardContext(self.to_do_list, self.to_do_list, self.to_do_list, self.to_do_list, self.to_do_cards, self.to_do_cards, self.to_do_cards, self.to_do_cards)
         self.context.action_history = self.action_history
 
         self.navbar = Navbar(self.root, None, None)
 
+    #Testing undo by adding a card, then applying undo and checking if card not in list anymore
     def testUndoAdd(self):
         new_card_data = Cards.create_card_data("Test Card", ["Task 1", "Task 2"], "red", 1)
         self.to_do_cards.append(new_card_data)
+        #Recording the action so can undo it later
         self.context.action_history.record_action(Action(new_card_data, self.to_do_cards, None, action_type="add"))
 
         self.context.action_history.undo(self.context)
-        self.context.action_history.redo(self.context)
-        self.assertIn(new_card_data, self.to_do_cards)
+        self.assertNotIn(new_card_data, self.to_do_cards)
 
-
+    #Testing redo by repeating last test, but then adding redo at the end and checking if now card inside list
     def testRedoUndo(self):
         new_card_data = Cards.create_card_data("Test Card", ["Task 1", "Task 2"], "red", 1)
         self.to_do_cards.append(new_card_data)
         self.context.action_history.record_action(Action(new_card_data, self.to_do_cards, None, action_type="add"))
 
+        #Undo then redo
+        self.context.action_history.undo(self.context)
         self.context.action_history.redo(self.context)
         self.assertIn(new_card_data, self.to_do_cards)
 
+    #Testing if the help window is generated when button pressed
     def testHelpButton(self):
         self.navbar.show_help_window()
         self.assertTrue(self.navbar.help_window_open)
 
-
+    #General tear down to remove roots after testing
     def tearDown(self):
         self.root.destroy()
 
