3e8d13fac0aeeae950605e127662d30be39b95fc
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2023-12-07 13:54:09 +0000

    Adding some more setting menu tests to check if they work e.g checking if background image is removed in high contrast mode.

--- finance_tracker/src/test/SettingsMenu.test.js
+++ finance_tracker/src/test/SettingsMenu.test.js
@@ -1,15 +1,58 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom'; 
 import SettingsMenu from '../components/SettingsMenu';
+import App from "../App";
+
 import '@testing-library/jest-dom';
 
-test('Settings element loads', () => {
-  render(
-    <MemoryRouter>
-      <SettingsMenu />
-    </MemoryRouter>
-  );
-  const settingsMenu = screen.getByAltText("Settings")
-  expect(settingsMenu).toBeInTheDocument();
+test('Setting elements loading', () => {
+    render(
+        <MemoryRouter>
+            <SettingsMenu />
+        </MemoryRouter>
+    );
+    const settingsMenu = screen.getByAltText("Settings")
+    expect(settingsMenu).toBeInTheDocument();
+
+});
+
+test('Enlarge text checkbox', () => {
+    render(
+        <MemoryRouter>
+            <App />
+        </MemoryRouter>
+    );
+    const enlargeTextCheckbox = screen.getByText('Enlarge Text').closest('label').querySelector('input');
+    fireEvent.click(enlargeTextCheckbox);
+  
+    const homePageText = screen.getByText('Financial');
+    expect(homePageText).toHaveStyle('font-size: 2em');
+});
+test('High Contrast Mode checkbox', () => {
+    render(
+        <MemoryRouter>
+            <App />
+        </MemoryRouter>
+    );
+
+    const highContrastCheckbox = screen.getByText('High Contrast Mode').closest('label').querySelector('input');
+    fireEvent.click(highContrastCheckbox);
+
+    const bodyElement = document.body;
+    expect(bodyElement).toHaveStyle('background-image:');
+});
+
+test('Tritanopia checkbox', () => {
+    render(
+        <MemoryRouter>
+            <App />
+        </MemoryRouter>
+    );
+
+    const highContrastCheckbox = screen.getByText('Tritanopia (Blue-Blind)').closest('label').querySelector('input');
+    fireEvent.click(highContrastCheckbox);
+
+    const bodyElement = screen.getByText('Need help?');
+    expect(bodyElement).toHaveStyle('color: rgb(8, 217, 214), #BE9FE1');
 });
