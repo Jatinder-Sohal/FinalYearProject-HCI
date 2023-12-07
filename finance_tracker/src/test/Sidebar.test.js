58a408ac1a55f98b32c5533f69c699977e34eff5
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2023-12-07 11:47:30 +0000

    Adding 2 more simple tests to Sidebar, checking if correct functions are called when events happen.

--- finance_tracker/src/test/Sidebar.test.js
+++ finance_tracker/src/test/Sidebar.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom'; 
-
+import { useState } from 'react';
 import Sidebar from '../components/Sidebar';
 
 test('Sheets in sidebar', () => {
@@ -11,5 +11,24 @@ test('Sheets in sidebar', () => {
     sheets.forEach((sheet) => {
       expect(screen.getByText(sheet)).toBeInTheDocument();
     });
-  });
-  
+});
+test('Delete function called with correct argument', () => {
+  const sheets = ['Sheet1', 'Sheet2', 'Sheet3'];
+  const fakeRemove = jest.fn();
+  render(<Sidebar sheets={sheets} RemoveSheet={fakeRemove} />);
+
+  fireEvent.click(screen.getAllByAltText('Delete sheet')[0]);
+
+  expect(fakeRemove).toHaveBeenCalledWith('Sheet1');
+});
+test('OnSheetClick function called on sheet click', () => {
+  const sheets = ['Sheet1', 'Sheet2', 'Sheet3'];
+  const fakeOnSheetClick = jest.fn();
+  render(<Sidebar sheets={sheets} OnSheetClick={fakeOnSheetClick} />);
+
+  fireEvent.click(screen.getByText('Sheet2'));
+
+  expect(fakeOnSheetClick).toHaveBeenCalledWith('Sheet2');
+});
+
+   
