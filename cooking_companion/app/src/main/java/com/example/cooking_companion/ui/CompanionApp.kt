de905888844df3ef4a776c1f4ebb47c54a4a1a3f
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2024-01-20 11:56:29 +0000

    Created new android project and set up ui package with folders that will contain future files.

---
+++ cooking_companion/app/src/main/java/com/example/cooking_companion/ui/CompanionApp.kt
@@ -0,0 +1,9 @@
+package com.example.cooking_companion.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CompanionApp(
+    modifier: Modifier = Modifier,
+) {}
