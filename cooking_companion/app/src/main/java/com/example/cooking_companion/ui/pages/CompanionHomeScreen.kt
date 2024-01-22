4042a4670e3342df58f94b22e71d13e0deb7efc7
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2024-01-22 16:44:37 +0000

    Added a basic search bar to the home screen using a surface component and placed a search icon inside.

--- cooking_companion/app/src/main/java/com/example/cooking_companion/ui/pages/CompanionHomeScreen.kt
+++ cooking_companion/app/src/main/java/com/example/cooking_companion/ui/pages/CompanionHomeScreen.kt
@@ -1,12 +1,70 @@
 package com.example.cooking_companion.ui.pages
 
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CompanionHomeScreen(
     modifier: Modifier = Modifier,
 ) {
-    Text(text = "home")
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .background(Color.Blue)
+            .fillMaxHeight()
+            .fillMaxWidth()
+    ){
+        SearchBar()
+    }
+
+}
+
+@Composable
+fun SearchBar(modifier: Modifier = Modifier){
+    Surface(
+        modifier = modifier
+            .height(80.dp)
+            .width(280.dp)
+            .padding(vertical = 20.dp),
+
+        border = BorderStroke(2.dp, Color.Black),
+        shape = RoundedCornerShape(50),
+
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search Icon",
+                modifier = Modifier.size(24.dp)
+            )
+        }}
 }
