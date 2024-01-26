7c24ce2effa175575a9d6b6ef1140406486b1883
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2024-01-26 17:38:43 +0000

    Added indicators under the pictures, and uses rounded shape to indicate which one is currently selected. Also added more suitable pictures instead of test placements.

--- cooking_companion/app/src/main/java/com/example/cooking_companion/ui/components/RecipiesCarousel.kt
+++ cooking_companion/app/src/main/java/com/example/cooking_companion/ui/components/RecipiesCarousel.kt
@@ -2,14 +2,25 @@ package com.example.cooking_companion.ui.components
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -19,9 +30,9 @@ import com.example.cooking_companion.R
 @Composable
 fun RecipeCarousel(modifier : Modifier = Modifier) {
     val images = listOf(
-        R.drawable.test1,
-        R.drawable.test,
-        R.drawable.asian_cat
+        R.drawable.fish_recent,
+        R.drawable.grilledcheese_recent,
+        R.drawable.pancakes_recent
     )
     val pagerState = rememberPagerState(pageCount = {images.size})
 
@@ -29,9 +40,7 @@ fun RecipeCarousel(modifier : Modifier = Modifier) {
         state = pagerState,
         modifier = modifier
             .fillMaxWidth()
-            .height(200.dp)
-
-
+            .height(230.dp)
     ) { page ->
         Image(
             painter = painterResource(id = images[page]),
@@ -39,7 +48,44 @@ fun RecipeCarousel(modifier : Modifier = Modifier) {
             contentScale = ContentScale.Crop,
             modifier = Modifier
                 .fillMaxWidth()
-                .height(200.dp)
+                .height(220.dp)
+                .padding(13.dp)
+                .clip(RoundedCornerShape(percent = 10))
         )
     }
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .align(Alignment.Center)
+        ) {
+            images.forEachIndexed { index, _ ->
+                CarouselIndicator(isSelected = pagerState.currentPage == index)
+            }
+        }
+    }
+}
+
+@Composable
+fun CarouselIndicator(isSelected: Boolean, modifier : Modifier = Modifier) {
+    if (isSelected)
+        Box(
+            modifier = modifier
+                .padding(horizontal = 4.dp)
+                .width(45.dp)
+                .height(14.dp)
+                .clip(RoundedCornerShape(percent = 50))
+                .background(Color.Black),
+        )
+    else
+        Box(
+            modifier = Modifier
+                .padding(horizontal = 4.dp)
+                .size(12.dp)
+                .clip(CircleShape)
+                .background(Color.Gray)
+        )
 }
