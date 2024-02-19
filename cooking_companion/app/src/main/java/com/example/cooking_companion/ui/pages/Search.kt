32a9942e8025c1bf82e348b38e25aac15a06beff
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2024-02-19 10:25:18 +0000

    Add 2 subheadings with contain recent searches and pouplar searches. Looped through arrays to display each subheading results.

--- cooking_companion/app/src/main/java/com/example/cooking_companion/ui/pages/Search.kt
+++ cooking_companion/app/src/main/java/com/example/cooking_companion/ui/pages/Search.kt
@@ -1,7 +1,9 @@
 package com.example.cooking_companion.ui.pages
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -24,6 +26,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
 
@@ -31,7 +34,8 @@ import androidx.navigation.NavHostController
 @Composable
 fun Search(navController: NavHostController, modifier: Modifier = Modifier) {
     var searchQuery by remember { mutableStateOf("") }
-
+    val recentSearches = listOf("Test1", "Test2", "Sest3")
+    val popularSearches = listOf("Test1", "Test2", "Test3", "Test4", "test5")
     Scaffold(
         topBar = {
             Row(
@@ -42,6 +46,7 @@ fun Search(navController: NavHostController, modifier: Modifier = Modifier) {
                 Icon(
                     imageVector = Icons.Default.ArrowBackIos,
                     contentDescription = "Back arrow",
+                    tint = MaterialTheme.colorScheme.primary,
                     modifier = modifier
                         .height(95.dp)
                         .padding(start = 22.dp)
@@ -51,7 +56,7 @@ fun Search(navController: NavHostController, modifier: Modifier = Modifier) {
                     onValueChange = { searchQuery = it },
                     colors = TextFieldDefaults.outlinedTextFieldColors(
                         unfocusedBorderColor = Color.LightGray,
-                        focusedBorderColor = Color.LightGray,
+                        focusedBorderColor = MaterialTheme.colorScheme.onBackground,
                         containerColor = Color.Transparent
                     ),
                     shape = RoundedCornerShape(15),
@@ -72,14 +77,61 @@ fun Search(navController: NavHostController, modifier: Modifier = Modifier) {
                         .padding(horizontal = 26.dp, vertical = 6.dp)
                 )
             }
+
         },
     ) { innerPadding ->
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = modifier
                 .padding(innerPadding)
         ) {
-
+            Text(
+                text = "Recent Searches",
+                fontSize = 24.sp,
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.titleSmall,
+                modifier = modifier
+                    .padding(horizontal = 20.dp, vertical = 4.dp)
+            )
+            recentSearches.forEach { searchTerm ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable {  }
+                        .padding(horizontal = 24.dp, vertical = 10.dp)
+                ) {
+                    Text(
+                        text = searchTerm,
+                        color = MaterialTheme.colorScheme.onSurface,
+                        style = MaterialTheme.typography.labelMedium,
+                        fontSize = 20.sp,
+                    )
+                }
+            }
+            Text(
+                text = "Popular Searches",
+                fontSize = 24.sp,
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.titleSmall,
+                modifier = modifier
+                    .padding(horizontal = 20.dp, vertical = 4.dp).padding(top = 8.dp)
+            )
+            popularSearches.forEach { searchTerm ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable {  }
+                        .padding(horizontal = 24.dp, vertical = 10.dp)
+                ) {
+                    Text(
+                        text = searchTerm,
+                        color = MaterialTheme.colorScheme.onSurface,
+                        style = MaterialTheme.typography.labelMedium,
+                        fontSize = 20.sp,
+                    )
+                }
+            }
         }
     }
 }
