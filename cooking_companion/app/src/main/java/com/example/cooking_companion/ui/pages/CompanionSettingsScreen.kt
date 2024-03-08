63ad65d939eb2f11ea41773243210a59f8dc59b0
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2024-03-08 13:21:57 +0000

    Seperating settings into their own sections and adding some example settings.

--- cooking_companion/app/src/main/java/com/example/cooking_companion/ui/pages/CompanionSettingsScreen.kt
+++ cooking_companion/app/src/main/java/com/example/cooking_companion/ui/pages/CompanionSettingsScreen.kt
@@ -4,6 +4,8 @@ package com.example.cooking_companion.ui.pages
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
@@ -14,19 +16,22 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.cooking_companion.data.DataSource.darkMode
 
 @Composable
-fun CompanionSettingsScreen(
-    modifier: Modifier = Modifier,
-) {
+fun CompanionSettingsScreen(modifier: Modifier = Modifier, ) {
+    val scrollState = rememberScrollState()
     val notificationsEnabled = remember { mutableStateOf(true) }
     val darkThemeEnabled = remember { mutableStateOf(darkMode.value) }
 
     Column(
-        modifier = modifier.padding(horizontal = 16.dp, vertical = 4.dp)
+        modifier = modifier
+            .padding(horizontal = 16.dp, vertical = 4.dp)
+            .verticalScroll(scrollState)
     ) {
         Text(
             text = "Settings",
@@ -37,18 +42,27 @@ fun CompanionSettingsScreen(
                 .padding(top = 20.dp)
                 .align(Alignment.Start)
         )
+        Divider()
+        Text(
+            text = "System",
+            style = MaterialTheme.typography.headlineSmall,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = modifier
+                .padding(horizontal = 16.dp, vertical = 1.dp)
+                .padding(top = 15.dp)
+                .align(Alignment.Start)
+        )
 
         ListItem(
             headlineContent = { Text("Notifications") },
             trailingContent = {
                 Switch(
-                    checked = notificationsEnabled.value,
-                    onCheckedChange = { notificationsEnabled.value = it }
+                    checked = darkThemeEnabled.value,
+                    onCheckedChange = { darkMode.value =  !darkMode.value; darkThemeEnabled.value = it }
                 )
             }
         )
-        Divider()
-
         ListItem(
             headlineContent = { Text("Dark Theme") },
             trailingContent = {
@@ -58,7 +72,85 @@ fun CompanionSettingsScreen(
                 )
             }
         )
+
         Divider()
+        Text(
+            text = "Accessibility",
+            style = MaterialTheme.typography.headlineSmall,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = modifier
+                .padding(horizontal = 16.dp, vertical = 1.dp)
+                .padding(top = 15.dp)
+                .align(Alignment.Start)
+        )
+        ListItem(
+            headlineContent = { Text("Enlarged Text") },
+            trailingContent = {
+                Switch(
+                    checked = darkThemeEnabled.value,
+                    onCheckedChange = { darkMode.value =  !darkMode.value; darkThemeEnabled.value = it }
+                )
+            }
+        )
+        ListItem(
+            headlineContent = { Text("Contrast mode") },
+            trailingContent = {
+                Switch(
+                    checked = darkThemeEnabled.value,
+                    onCheckedChange = { darkMode.value =  !darkMode.value; darkThemeEnabled.value = it }
+                )
+            }
+        )
+        Divider()
+        Text(
+            text = "Customization",
+            style = MaterialTheme.typography.headlineSmall,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = modifier
+                .padding(horizontal = 16.dp, vertical = 1.dp)
+                .padding(top = 15.dp)
+                .align(Alignment.Start)
+        )
+        ListItem(
+            headlineContent = { Text("Theme") },
+            trailingContent = {
+                Switch(
+                    checked = darkThemeEnabled.value,
+                    onCheckedChange = { darkMode.value =  !darkMode.value; darkThemeEnabled.value = it }
+                )
+            }
+        )
+        ListItem(
+            headlineContent = { Text("Measurements") },
+            trailingContent = {
+                Switch(
+                    checked = darkThemeEnabled.value,
+                    onCheckedChange = { darkMode.value =  !darkMode.value; darkThemeEnabled.value = it }
+                )
+            }
+        )
+        ListItem(
+            headlineContent = { Text("Auto Servings") },
+            trailingContent = {
+                Switch(
+                    checked = darkThemeEnabled.value,
+                    onCheckedChange = { darkMode.value =  !darkMode.value; darkThemeEnabled.value = it }
+                )
+            }
+        )
+        Divider()
+        Text(
+            text = "Other",
+            style = MaterialTheme.typography.headlineSmall,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = modifier
+                .padding(horizontal = 16.dp, vertical = 1.dp)
+                .padding(top = 15.dp)
+                .align(Alignment.Start)
+        )
 
     }
 }
