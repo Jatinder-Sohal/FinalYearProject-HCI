323da0e84f994ebcc110a18a0a2761c5f9af6aa3
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2024-03-08 23:32:03 +0000

    Adjust css of nearly all elements to make it more appealing. Also split plus away to add specfic css like increase size.

--- project_planning_new/src/components/list.jsx
+++ project_planning_new/src/components/list.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import './list.css'; 
 
 const List = ({ title }) => {
-  return (
-    <div className="list">
-      <header className="list-header">
-        <h2>{title}</h2>
-        <button className="list-action">...</button>
-      </header>
-      <button className="add-card-btn">+ Add a card</button>
-    </div>
-  );
+    return (
+        <div className="list">
+            <header className="list-header">
+                <h2>{title}</h2>
+                <button className="list-action">...</button>
+            </header>
+            <button className="add-card-btn">
+                <span className="plus-icon">+</span>
+                <span className="add-card-btn-text">Add a card</span>
+            </button>
+        </div>
+    );
 };
 
 export default List;
