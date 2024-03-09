d93c07899e89d0448801aa53a2978354f55c17c9
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2024-03-09 12:44:25 +0000

    Created a card design and printed outside using simple map function and hook to allow future additions.

--- project_planning_new/src/components/list.jsx
+++ project_planning_new/src/components/list.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { useState } from 'react';
 import './list.css'; 
 
 const List = ({ title }) => {
+    const [todoCards, setCards] = useState([
+      { id: 1, title: 'Sample Card' }, 
+      { id: 2, title: 'Testing a longer card and text to see if overflow works' }, 
+    ]);
+    
+    
     return (
         <div className="list">
             <header className="list-header">
                 <h2>{title}</h2>
                 <button className="list-action">...</button>
             </header>
+        
+            {todoCards.map(card => (
+                <div key={card.id} className="card">
+                    {card.title}
+                </div>
+            ))}
             <button className="add-card-btn">
                 <span className="plus-icon">+</span>
                 <span className="add-card-btn-text">Add a card</span>
