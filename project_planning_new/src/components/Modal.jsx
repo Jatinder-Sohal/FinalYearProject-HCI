52a5e71f821afc46166d9ffb94f9fe977a6d02cf
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2024-03-11 13:22:20 +0000

    Added a description and checkbox title and respective textarea. Checklist textarea was to test the scrollbar of the modal.

--- project_planning_new/src/components/Modal.jsx
+++ project_planning_new/src/components/Modal.jsx
@@ -10,6 +10,10 @@ const Modal = ({ card, onClose, listTitle }) => {
             <img src={Cancel} className="modal-close"onClick={onClose}  alt="Close Modal" />
           </div>
           <h2 className='modal-listFrom'>In list: {listTitle}</h2>
+          <h3 className='title-description'>Description</h3>
+          <textarea className="modal-description"  placeholder="Enter a description for this card..."></textarea>
+          <h3 className='title-description'>Checklist</h3>
+          <textarea className="modal-description"  placeholder="Enter a description for this card..."></textarea>
         </div>
       </div>
     );
