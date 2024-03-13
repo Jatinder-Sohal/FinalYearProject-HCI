40c5465aa0d3685a073b18ce0211b5db81e0fc46
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2024-03-13 13:48:28 +0000

    Made multiple changes, first splitting topbar into 3 sections using flex, changing padding of toolbar, add some information about the board in top left with a favourite icon with respective svg's added to file system.

--- project_planning_new/src/components/Toolbar.jsx
+++ project_planning_new/src/components/Toolbar.jsx
@@ -9,13 +9,13 @@ function Toolbar(){
     return(
         <div className='Toolbar'>
             <div className='Main-Toolbar'>
-                <img src={Undo} className="Item-Toolbar"   alt="Undo last actions" />
-                <img src={Redo} className="Item-Toolbar"   alt="Redo last actions" />
-                <img src={Sort} className="Item-Toolbar"   alt="Sort cards" />
-                <img src={Filter} className="Item-Toolbar"   alt="Filter cards" />
+                <img src={Undo} className="Item-Toolbar" alt="Undo last actions" />
+                <img src={Redo} className="Item-Toolbar" alt="Redo last actions" />
+                <img src={Sort} className="Item-Toolbar" alt="Sort cards" />
+                <img src={Filter} className="Item-Toolbar" alt="Filter cards" />
                 </div>
             <div className='Single-Toolbar'>
-                <img src={Delete} className="Item-Toolbar"   alt="Delete all cards" />
+                <img src={Delete} className="Item-Toolbar" alt="Delete all cards" />
             </div>
         </div>
     );
--- project_planning_new/src/components/Topbar.jsx
+++ project_planning_new/src/components/Topbar.jsx
@@ -1,11 +1,21 @@
 import './Topbar.css'
+import Star from '../images/star.png';
 
 function Topbar(){
     return(
         <div className='Topbar'>
-        <h1 className='Topbar-Title'>Kanban Board</h1>
-            <div>
-                
+            <div className='left-Topbar'>
+                <div className='Board-Information'>
+                    <h2 className='date'>First Created: 5/03/2024</h2>
+                    <h2 className='date' style={{marginBottom: "30px"}}>Last Accessed: 5/03/2024</h2>
+                </div>
+                <img src={Star} className="Star" alt="Favourite Board" />
+            </div>
+            <div className='middle-Topbar'>
+                <h1 className='Topbar-Title'>Kanban Board</h1>
+            </div>
+            <div className='right-Topbar'>
+                <h1 className='Topbar-Title'>Kanban Board</h1>
             </div>
         </div>
     );
