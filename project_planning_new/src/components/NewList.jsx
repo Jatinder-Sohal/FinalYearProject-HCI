b86909c05eddd52b8c0400d2058ceaf9e9898b23
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2024-03-14 16:43:49 +0000

    Created new css file for newlist items and added the code for when new list button is pressed. Edited css for all items involved.

--- project_planning_new/src/components/NewList.jsx
+++ project_planning_new/src/components/NewList.jsx
@@ -1,21 +1,27 @@
 import { useState } from 'react';
 import './List.css'
+import './NewList.css'
+import Cancel from '../images/cancel-svgrepo-com.png';
 
 function NewList(){
     const [isAddingCard, setIsAddingCard] = useState(false);
 
     return(
-        <div className="NewList">
-            <div>
+        <div>
             {isAddingCard ? (
-                    <div />
+                <div className="NewList">
+                    <textarea className="new-list-input" placeholder="Enter list title..."></textarea>
+                    <div style={{ display: 'flex', marginTop:'2px', alignItems: 'center'}}>
+                        <button className="btn btn-primary confirm-card-button">Add Card</button>
+                        <img src={Cancel} className="cancel-card-button"   alt="Cancel card" />
+                    </div>
+                </div>
             ) : (
-                <button className="add-card-btn" onClick={() => setIsAddingCard(true)}>
+                <button className="NewList-add" onClick={() => setIsAddingCard(true)}>
                     <span className="plus-icon">+</span>
-                    <span className="add-card-btn-text">Add a card</span>
+                    <span className="add-card-btn-text">Add new list</span>
                 </button>
             )}
-            </div>
         </div>
     );
 }
