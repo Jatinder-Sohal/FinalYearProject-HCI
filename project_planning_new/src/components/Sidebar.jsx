d1ae29f92f0086403ada568985e97a772238c9d6
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2024-03-16 14:22:40 +0000

    Adding images for the dropdown and star and then adding them to the different sidebar elements. Dropdown changes shape using state variable.

--- project_planning_new/src/components/Sidebar.jsx
+++ project_planning_new/src/components/Sidebar.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import './Sidebar.css';
-
-
+import DropdownUp from '../images/dropdown-up.png';
+import DropdownDown from '../images/dropdown-down.png';
+import StarFilled from '../images/star.png'
+import Star from '../images/filled-star.png'
+import StarBorder from '../images/star-border.png'
+import BinBlack from '../images/bin-black.png'
 
 function Sidebar(){
   const favoriteBoards = ['Board 1', 'Board 2']; 
@@ -9,12 +13,24 @@ function Sidebar(){
   
   const [isAllOpen, setIsAllOpen] = useState(true);
   const [isFavoriteOpen, setIsFavOpen] = useState(true);
+  const [favImage, setFavImage] = useState(DropdownUp);
+  const [allImage, setAllImage] = useState(DropdownUp);
 
   const toggleAllSection = () => {
     setIsAllOpen(!isAllOpen);
+    if (allImage == DropdownDown){
+        setAllImage(DropdownUp)
+    }else{
+        setAllImage(DropdownDown)
+    }
   };
   const toggleFavSection = () => {
     setIsFavOpen(!isFavoriteOpen);
+    if (favImage == DropdownDown){
+        setFavImage(DropdownUp)
+    }else{
+        setFavImage(DropdownDown)
+    }
   };
 
   return (
@@ -22,24 +38,33 @@ function Sidebar(){
         <div>
             <div className="fav-section">
                 <div className="fav-section-header" onClick={toggleFavSection}>
+                    <img className="dropdown-images" src={favImage}/>
                     {"Favorite Boards"}
                 </div>
                 {isFavoriteOpen &&
                     <ul className='lists'>
                         {favoriteBoards.map(board => (
-                            <div className="section-item" key={board}>{board}</div>
+                            <div className="section-item" key={board}>
+                                {board}
+                                <img className="dropdown-item-images" src={StarFilled}/>
+                            </div>
                         ))}
                     </ul>
                 }                    
             </div>   
             <div className="all-section">
                 <div className="all-section-header" onClick={toggleAllSection}>
+                    <img className="dropdown-images" src={allImage}/>
                     {"All Boards"}
                 </div>
                 {isAllOpen &&
                     <ul className='lists'>
                         {allBoards.map(board => (
-                            <div className="section-item" key={board}>{board}</div>
+                            <div className="section-item" key={board}>
+                                {board}
+                                <img className="dropdown-item-images" src={BinBlack}/>
+                            </div>
+                            
                         ))}
                     </ul>
                 }                    
