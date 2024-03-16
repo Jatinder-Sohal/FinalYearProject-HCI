b8848963595a6c3a0909266690732baeb4e10dba
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2024-03-16 13:08:24 +0000

    Started adding content to sidebar with a favourite board div that expands when clicked showing some list items using map function and state variables.

--- project_planning_new/src/components/Sidebar.jsx
+++ project_planning_new/src/components/Sidebar.jsx
@@ -1,10 +1,41 @@
-import './Sidebar.css'
+import React, { useState } from 'react';
+import './Sidebar.css';
+
+
 
 function Sidebar(){
-    return(
-        <div className='Sidebar'>
+  const favoriteBoards = ['Board 1', 'Board 2']; 
+  const allBoards = ['Board 3', 'Board 4', 'Board 5'];
+  
+  const [isAllOpen, setIsAllOpen] = useState(true);
+  const [isFavoriteOpen, setIsFavOpen] = useState(true);
+
+  const toggleAllSection = () => {
+    setIsAllOpen(!isAllOpen);
+  };
+  const toggleFavSection = () => {
+    setIsFavOpen(!isFavoriteOpen);
+  };
+  
+  return (
+    <div className="sidebar">
+        <div style={{marginTop: "70px"}}>
+            <div className="fav-section">
+                <div className="fav-section-header" onClick={toggleFavSection}>
+                    {"Favorite Boards"}
+                </div>
+                {isFavoriteOpen &&
+                    <ul>
+                        {favoriteBoards.map(board => (
+                            <li key={board}>{board}</li>
+                        ))}
+                    </ul>
+                }                    
+            </div>   
+            
         </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Sidebar;
